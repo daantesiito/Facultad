6a3b193e2f20c6d9ae770e109fb94339432192b5
Dante Puddu <89815375+daantesiito@users.noreply.github.com>
2024-04-04 14:29:05 -0300

    ayed

---
+++ 2do año/Quinto Semestre/AYED/TutorialEclipse/Practicas/practica1/ejercicio7g.java
@@ -0,0 +1,23 @@
+package practica1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ejercicio7g {
+	public List<Integer> calcularSucesion(int n) {
+		List<Integer> sucesion = new ArrayList<>();
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Ingrese un numero: ");
+		
+		int numero = scanner.nextInt();
+		
+		if (numero % 2) {
+			
+		}
+		
+		return sucesion;
+	}
+}
