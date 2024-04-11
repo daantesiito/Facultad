f763ed2b5acd456c63cc09cd6d9ded202de6d116
Dante Puddu <dantepuddu@hotmail.com>
2024-04-11 14:57:25 -0300

    Python entrega p2

--- 2do año/Quinto Semestre/PYTHON/Practica/Practica 2/funciones.py
+++ 2do año/Quinto Semestre/PYTHON/Practica/Practica 2/funciones.py
@@ -1,5 +1,5 @@
-def GetStats(names, goals, goals_avoided, assists):
-    stats = tuple(zip(names, goals, goals_avoided, assists))
+def GetStats(split_names, goals, goals_avoided, assists):
+    stats = tuple(zip(map(str.lower, split_names), goals, goals_avoided, assists))
     return stats
 
 
@@ -11,12 +11,12 @@ def GetMostInfluential(stats):
     return max(stats, key=lambda player: player[1] * 1.5 + player[2] * 1.25 + player[3])
 
 
-def GetGoalsAverage(goals):
+def GetGoalsAverage(goals,MATCHES):
     total_goals = sum(goals)
-    average = lambda x: x / 25
+    average = lambda x: x / MATCHES
     return average(total_goals)
 
 
-def GetGoleadorGoalsAverage(goals_scored):
-    average = lambda x: x / 25
+def GetGoleadorGoalsAverage(goals_scored,MATCHES):
+    average = lambda x: x / MATCHES
     return average(goals_scored)
