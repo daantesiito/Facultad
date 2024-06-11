dc015bddfd5cc4683594321c49893369bcf36906
Dante Puddu <dantepuddu@hotmail.com>
2024-06-11 02:18:19 -0300

    ayed

--- 2do año/Quinto Semestre/PYTHON/Practicas/Practica5 PYTHON PLUS/TP5PyPlus.py
+++ 2do año/Quinto Semestre/PYTHON/Practicas/Practica5 PYTHON PLUS/TP5PyPlus.py
@@ -1,24 +1,75 @@
-import csv
+def funcionOriginal():
+    import csv
 
-file_route = "C:/Facultad/2do año/Quinto Semestre/PYTHON/Practicas/Practica5 PYTHON PLUS/area_protegida.csv"
+    file_route = "./lagos_arg.csv"
 
-def print_report(title, *args):
-    print(f"{title.capitalize():-^40}")
-    for elem in args:
-        print(f"{elem[0]}: {elem[1]}")
+    def my_function(data, *args):
+        sub_data = filter(lambda x: x[1] in args and x[6]=="medio", data)
+        return list(sub_data)
 
-with open(file_route, "r", encoding="utf-8") as data_set:
-    reader = csv.reader(data_set)
-    header, data = next(reader), list(reader)
+    with open(file_route, "r", encoding="utf-8") as data_set:
+        reader = csv.reader(data_set)
+        header, data = next(reader), list(reader)
 
-my_data = {}
+    result1 = my_function(data, "Chubut", "Río Negro")
+    result2 = my_function(data, "Neuquén")
 
-for row in data:
-    num = int(row[13]) if row[13]!="" else 0
-    if row[8] in my_data:
-        my_data[row[8]] += num
-    else:
-        my_data[row[8]] = num
-        
-top_rating = sorted(my_data.items(), key=lambda x: x[1], reverse=True)[:5]
-print_report("Super listado", top_rating[0], top_rating[1], top_rating[2])
+    print(result1)
+    print(f"\n****\n{result2}")
+
+############################################################################################
+# Solucion con PANDAS
+
+def funcionPandas():
+    import pandas as pd
+
+    file_route = "./lagos_arg.csv"
+
+    data = pd.read_csv(file_route) #DATAFRAME
+
+    def my_function(data, *args):
+        tamaños = data[(data.iloc[:, 1].isin(args)) & (data.iloc[:, 6] == "medio")] 
+        return tamaños
+
+    result1 = my_function(data, "Chubut", "Río Negro")
+    result2 = my_function(data, "Neuquén")
+
+    print(result1)
+    print(f"\n****\n{result2}")
+
+    tipos = data.dtypes # SERIE
+
+    print(tipos)
+
+############################################################################################
+# A:
+funcionOriginal()
+
+print(' ------------------------------------------------ ')
+
+# B:
+funcionPandas()
+# ILOC itera sobre filas y columnas: Si se pone ":", se leen todas las filas o todas las columnas.
+# data.iloc[:,1]: Se leen todas las filas de la columna 1. En este caso, es la columna de Ubicación.
+# .isin(args): Toma los argumentos que se pasan a la función y verifica si están en alguna fila de la columna 1.
+# Primera condición: Se cumple si alguna fila de la columna 1 coincide con los argumentos.
+# data.iloc[:, 6]: Recorre todas las filas de la columna 6, que es la columna de Sup Tamaños.
+# Segunda condición: Se cumple si alguna fila de la columna 6 contiene el valor 'medio'.
+# Resultado: Todo esto se guarda en un nuevo DataFrame llamado tamaños.
+
+# C:
+# Tipo DATAFRAME: data
+# Tipo SERIES: tipos
+
+# D:
+# tipos = data.dtypes 
+#
+# print(tipos)
+
+# E:
+# 1- Pandas esta preparado para usar datasets grandes y funciona perfecto con muchos formatos, JSON, SQL, CSV, etc.
+# 2- Tiene funciones que son mucho mas eficientes y faciles de usar que las operaciones basicas,
+#    las cuales dan ventajas en optimizacion, indexacion, seleccion y lectura del programa.
+# 3- Pandas se complementa muy bien con otras librerias de python y hace mas facil el trabajo complementado.
+#    EJ: Matplotlib
+# 4- Pandas tambien puede juntar 2 datasets o mas, utilizando el merge(), para trabajar todo junto.
